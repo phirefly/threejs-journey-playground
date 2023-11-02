efe6230b1c34c69ac35156794a80bb193c0e9ac6
Raphael Villas <raphael.villas@gmail.com>
2023-11-01 19:09:20 -0500

    Added lil-gui for changing values

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from 'lil-gui'
+
 
 /**
  * Base
@@ -18,6 +20,15 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+/**
+ *
+ */
+const gui = new GUI();
+gui.add('Hello World')
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation')
+gui.add(mesh.position, 'x').min(-3).max(3).step(0.01).name('horizontal')
+gui.add(mesh.position, 'z').min(-3).max(3).step(0.01).name('depth')
 
 /**
  * Sizes
